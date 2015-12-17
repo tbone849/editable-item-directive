e46fd38a7e93aa163531ee43366308399e0e4c35
Tim Sarvey <timsarvey@gmail.com>
2015-12-16 19:16:30 -0500

    Add unit test for directive controller.

--- app.js
+++ app.js
@@ -5,17 +5,17 @@ angular.module('myApp', [])
 			scope: true,
 			transclude: true,
 			templateUrl: 'editable.html',
-			link: function(scope, element, attrs){
-				scope.editOrSave = 'Edit';
-				scope.edit = false;
+			controller: function($scope, $element, $attrs){
+				$scope.editOrSave = 'Edit';
+				$scope.edit = false;
 
-				scope.toggleEditableRegion = function(){
-					if(scope.editOrSave === 'Edit'){ 
-						scope.editOrSave = 'Save'
-						scope.edit = true;
-					} else if(scope.editOrSave === 'Save') {
-						scope.editOrSave = 'Edit'
-						scope.edit = false;
+				$scope.toggleEditableRegion = function(){
+					if($scope.editOrSave === 'Edit'){ 
+						$scope.editOrSave = 'Save'
+						$scope.edit = true;
+					} else if($scope.editOrSave === 'Save') {
+						$scope.editOrSave = 'Edit'
+						$scope.edit = false;
 					}
 				}
 			}
--- makeEditableSpec.js
+++ makeEditableSpec.js
@@ -3,25 +3,42 @@ describe('makeEditable', function(){
 	var element;
 	var compiled;
 	var html;
+	var elementScope;
 
 	beforeEach(module('myApp'));
 	beforeEach(module('editable.html'));
 
 	beforeEach(inject(function($rootScope, $compile){
+
 		html = '';
 		html += '<div class="container" make-editable>';
 		html += '<p>Content that will become editable when rendered.</p>';
 		html += '</div>';		
 
 		scope = $rootScope.$new();
-        compiled = $compile(html)
+        compiled = $compile(html);
         element = compiled(scope);
         scope.$digest();
 	}));
 
 	it('should render the element correctly', function(){
 		expect(element.find('p').text()).toContain('rendered');
-		expect(element.find('p').parent().attr('contenteditable')).toBeTruthy();
-		expect(element.find('button').text()).toBe('Edit');
 	});
+
+	it('should expose a controller to toggle the editable region', function(){
+		elementScope = element.scope();
+		expect(angular.isFunction(elementScope.toggleEditableRegion)).toBe(true);
+		// default state
+		expect(elementScope.editOrSave).toBe('Edit');
+		expect(elementScope.edit).toBe(false);
+		// click edit button
+		elementScope.toggleEditableRegion();
+		expect(elementScope.editOrSave).toBe('Save');
+		expect(elementScope.edit).toBe(true);
+		// click save button
+		elementScope.toggleEditableRegion();
+		expect(elementScope.editOrSave).toBe('Edit');
+		expect(elementScope.edit).toBe(false);
+	});
+
 });
