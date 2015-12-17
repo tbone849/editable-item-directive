24827b3e748564c9173f27d0451318dd533744a1
Tim Sarvey <timsarvey@gmail.com>
2015-12-16 20:09:36 -0500

    Modify directive controller test to prove whether the content is editable or not.

--- makeEditableSpec.js
+++ makeEditableSpec.js
@@ -28,17 +28,25 @@ describe('makeEditable', function(){
 	it('should expose a controller to toggle the editable region', function(){
 		elementScope = element.scope();
 		expect(angular.isFunction(elementScope.toggleEditableRegion)).toBe(true);
+		
 		// default state
 		expect(elementScope.editOrSave).toBe('Edit');
 		expect(elementScope.edit).toBe(false);
+		expect(element.find('p').parent().attr('contenteditable')).toBe('false');
+		
 		// click edit button
 		elementScope.toggleEditableRegion();
+		scope.$digest();
 		expect(elementScope.editOrSave).toBe('Save');
 		expect(elementScope.edit).toBe(true);
+		expect(element.find('p').parent().attr('contenteditable')).toBe('true');
+		
 		// click save button
 		elementScope.toggleEditableRegion();
+		scope.$digest();
 		expect(elementScope.editOrSave).toBe('Edit');
 		expect(elementScope.edit).toBe(false);
+		expect(element.find('p').parent().attr('contenteditable')).toBe('false');
 	});
 
 });
